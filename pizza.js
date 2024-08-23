d11c873da1cebdf70a7edc7bb8680e142fb9ec43
kelvinkuria <kkuria383@yahoo.com>
2024-08-23 12:21:42 +0300

    Completed assignment

---
+++ pizza.js
@@ -0,0 +1,14 @@
+function calculatePizzaSize(size){
+     if (size ==="Extra-Large"){
+    return "15"
+     } else if (size === "Large"){
+        return "12"
+     }else if (size ==="Medium"){
+        return "10"
+     }else if (size === "Small"){
+        return "8"
+     }else{
+        return "Invalid size"
+     }
+
+}
